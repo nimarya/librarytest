6a19295edcf31917e6f96c142cc7d290b9d6470e
Maria <masha.nichiporenko@gmail.com>
2022-06-07 21:20:11 +0300

    changed to class

---
+++ Functions/Show.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace nimarya\Functions;
+
+class Show
+{
+    public function echohello()
+    {
+        echo 'hello';
+    }
+}
--- echohello.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function echohello()
-{
-    echo 'hello';
-}
